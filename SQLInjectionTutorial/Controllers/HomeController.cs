4fcba67af38969113b6fb3c891443ca46dd3c336
Ryan McBeth <ryan.mcbeth@gmail.com>
2022-01-01 15:31:33 -0500

    Initial commit

---
+++ SQLInjectionTutorial/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SQLInjectionTutorial.Models;
+using SQLInjectionTutorial.MvcApplication6.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQLInjectionTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            LoginModel obj = new LoginModel();
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult Index(LoginModel objuserlogin)
+        {
+            var display = Userloginvalues().Where(m => m.UserName == objuserlogin.UserName && m.UserPassword == objuserlogin.UserPassword).FirstOrDefault();
+            if (display != null)
+            {
+                ViewBag.Status = "CORRECT UserNAme and Password";
+            }
+            else
+            {
+                ViewBag.Status = "INCORRECT UserName or Password";
+            }
+            return View(objuserlogin);
+        }
+        public List<LoginModel> Userloginvalues()
+        {
+            List<LoginModel> objModel = new List<LoginModel>();
+            objModel.Add(new LoginModel { UserName = "user1", UserPassword = "password1" });
+            objModel.Add(new LoginModel { UserName = "user2", UserPassword = "password2" });
+            objModel.Add(new LoginModel { UserName = "user3", UserPassword = "password3" });
+            objModel.Add(new LoginModel { UserName = "user4", UserPassword = "password4" });
+            objModel.Add(new LoginModel { UserName = "user5", UserPassword = "password5" });
+            return objModel;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
